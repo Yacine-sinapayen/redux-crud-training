b20ef427e056ac5b91b32a89268361d57131d482
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2023-06-21 08:12:52 +0200

    post article data ok

--- src/reducers/post.reducer.js
+++ src/reducers/post.reducer.js
@@ -1,10 +1,12 @@
-import { GET_POSTS } from "../actions/post.action";
+import { GET_POSTS, ADD_POST } from "../actions/post.action";
 const initialState = {};
 
 export default function postReducer(state = initialState, action) {
   switch (action.type) {
     case GET_POSTS:
       return action.payload;
+    case ADD_POST:
+      return [action.payload, ...state];
     default:
       return state;
   }
