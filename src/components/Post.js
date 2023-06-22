396fa13de5b92ed89cfcead72922c681996cf8ce
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2023-06-22 07:28:47 +0200

    put article ok

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,31 @@
 import React, { useState } from "react";
 import Like from "./Like";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { isEmpty } from "./Utils";
+import { editPost } from "../actions/post.action";
 
 const Post = ({ post }) => {
   const [editToggle, setEditToggle] = useState(false);
   const user = useSelector((state) => state.userReducer);
+  const [editContent, setEditContent] = useState(post.content);
+  const dispatch = useDispatch();
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+
+    // Lorsque je modifie un élément d'une entité avec json server, il écrase tout le reste de l'elem contrairement à une bdd classique, donc je dois tout lui renvoyer.
+
+    const postData = {
+      title: post.title,
+      author: user.pseudo,
+      likes: post.likes,
+      id: post.id,
+      content: editContent,
+    };
+
+    dispatch(editPost(postData))
+    setEditToggle(false);
+  };
 
   return (
     <div className="post">
@@ -28,8 +48,12 @@ const Post = ({ post }) => {
       />
 
       {editToggle ? (
-        <form>
-          <textarea autoFocus={true} defaultValue={post.content}></textarea>
+        <form onSubmit={(e) => handleEdit(e)}>
+          <textarea
+            autoFocus={true}
+            defaultValue={post.content}
+            onChange={(e) => setEditContent(e.target.value)}
+          ></textarea>
           <input type="submit" value="Valider modification" />
         </form>
       ) : (
